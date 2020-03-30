aa36319a6c681da7808df39f64d563ad9966dbf9
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-03-30 16:21:30 -0400

    add CourseServiceClient and other empty component

--- client-angular-jiawen/src/app/app-routing.module.ts
+++ client-angular-jiawen/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CourseTableComponent} from "./course-table/course-table.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CourseTableComponent},
+  {path: 'courses', component: CourseTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-angular-jiawen/src/app/app.module.ts
+++ client-angular-jiawen/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseTableComponent } from './course-table/course-table.component';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import {CourseServiceClient} from "./services/CourseServiceClient";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseTableComponent
+    CourseTableComponent,
+    CourseViewerComponent,
+    LessonTabsComponent,
+    ModuleListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CourseServiceClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
