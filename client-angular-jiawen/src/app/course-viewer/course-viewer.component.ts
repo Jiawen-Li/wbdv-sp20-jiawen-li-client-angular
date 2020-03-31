a81aa2536cc5b2a2bc995e954c2a2a2f44e3dcbf
Jiawen-Li <li.jiawen5@husky.neu.edu>
2020-03-31 02:24:33 -0400

    course-viewer finish

--- client-angular-jiawen/src/app/course-viewer/course-viewer.component.ts
+++ client-angular-jiawen/src/app/course-viewer/course-viewer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {CourseServiceClient} from "../services/CourseServiceClient";
 
 @Component({
   selector: 'app-course-viewer',
@@ -8,13 +9,18 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class CourseViewerComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private service: CourseServiceClient) { }
 
   courseId = '';
+  course;
 
   ngOnInit(): void {
     this.route.params.subscribe(params=>{
       this.courseId = params.courseId;
+      this.service
+        .findCourseById(this.courseId)
+        .then(course => this.course = course)
     })
   }
 
